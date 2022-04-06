033e03b822b54eecb78778f8e86d5793ad7a281a
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2022-04-06 09:35:14 +0200

    init project

---
+++ src/views/question/new.php
@@ -0,0 +1,10 @@
+<h1> Ajouter une question </h1>
+<form action="" method="post" enctype="multipart/form">
+    <div class="form-group">
+       <label for="intitule">Intitulé</label>
+       <input type="text" name="intitule "id="intitule" class="form-control" placeholder="Intitulé">
+    </div>
+    <div class="text-end mb-3">
+        <button type="submit" class="btn btn-dark">Valider</button>
+    </div>
+</form>
